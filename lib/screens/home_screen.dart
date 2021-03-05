44e98a8b50cbfd2d6c47b26fc6e398d408853e6d
sehej <sehejjain@outlook.com>
2021-03-05 16:50:13 +0530

    Home Screen Optimization

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/models/icon_data.dart';
 import 'package:social_media_buttons/social_media_buttons.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'project_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,116 +18,161 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xff8ebeed),
       body: Container(
-        child: Center(
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Image.asset(
-                  'assets/sehej_new.png',
-                  fit: BoxFit.scaleDown,
-                ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Image.asset(
+                'assets/sehej_new.png',
+                fit: BoxFit.scaleDown,
               ),
-              Container(
-                height: size.height,
-                width: size.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: size.width * 0.1,
-                          ),
-                          Text(
-                            'Hey, I\'m Sehej',
-                            style: GoogleFonts.montserratAlternates(
-                              fontSize: 70,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ],
+            ),
+            Container(
+              height: size.height,
+              width: size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: size.width * 0.1,
                       ),
-                    ),
-                    Center(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: size.width * 0.1,
+                      Text(
+                        'Hey, I\'m Sehej',
+                        style: GoogleFonts.montserratAlternates(
+                          fontSize: 70,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: size.width * 0.1,
+                      ),
+                      Container(
+                        width: size.width * 0.5,
+                        child: Text(
+                          "I am a 20-year-old Computer Science Undergrad from New Delhi."
+                          "\nThe focus of my projects varies from building software solutions to building things for the internet, but what I prize myself for is the ability to learn new things and build upon them in my problem-solving process.",
+                          style: GoogleFonts.montserrat(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.1,
+                        ),
+                        SocialMediaIcon(
+                          iconData: SocialMediaIconData(
+                            icon: Icons.mail,
+                            link: "mailto:2018225@iiitdmj.ac.in",
                           ),
-                          SocialMediaButton.google(
-                            url: "mailto:2018225@gmail.com",
-                            size: 25,
-                            color: Colors.black54,
+                        ),
+                        SocialMediaIcon(
+                          iconData: SocialMediaIconData(
+                            icon: FontAwesomeIcons.linkedin,
+                            link: "https://www.linkedin.com/in/sehejjain/",
                           ),
-                          SocialMediaButton.linkedin(
-                            url: "https://www.linkedin.com/in/sehejjain/",
-                            size: 25,
-                            color: Colors.black54,
+                        ),
+                        SocialMediaIcon(
+                          iconData: SocialMediaIconData(
+                            icon: FontAwesomeIcons.github,
+                            link: "https://github.com/sehejjain",
                           ),
-                          SocialMediaButton.instagram(
-                            url:
+                        ),
+                        SocialMediaIcon(
+                          iconData: SocialMediaIconData(
+                            icon: FontAwesomeIcons.instagram,
+                            link:
                                 "https://www.instagram.com/sehej.on.the.offbeat/",
-                            size: 25,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Center(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.1,
+                        ),
+                        SizedBox(
+                          width: size.width * 0.1,
+                          height: size.height * 0.01,
+                          child: Container(
                             color: Colors.black54,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: size.height * 0.05,
-                    ),
-                    Center(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: size.width * 0.1,
-                          ),
-                          SizedBox(
-                            width: size.width * 0.1,
-                            height: size.height * 0.01,
-                            child: Container(
+                  ),
+                  Center(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.1,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ProjectScreen(),
+                                  ));
+                            });
+                          },
+                          hoverColor: Colors.transparent,
+                          child: Text(
+                            'Projects',
+                            style: GoogleFonts.montserratAlternates(
+                              fontSize: 35,
                               color: Colors.black54,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Center(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: size.width * 0.1,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => ProjectScreen(),
-                                    ));
-                              });
-                            },
-                            hoverColor: Colors.transparent,
-                            child: Text(
-                              'Projects',
-                              style: GoogleFonts.montserratAlternates(
-                                fontSize: 35,
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class SocialMediaIcon extends StatelessWidget {
+  final SocialMediaIconData iconData;
+  const SocialMediaIcon({
+    Key key,
+    this.iconData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 6.0, right: 6.0),
+      child: InkWell(
+        child: Icon(
+          iconData.icon,
+          size: 25,
+          color: Colors.black54,
+        ),
+        onTap: () {
+          launch(iconData.link);
+        },
       ),
     );
   }
