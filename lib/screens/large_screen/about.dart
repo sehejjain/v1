af569d8b88469e1278e5704c1d9c1aa4cc5e6728
sehej <sehejjain@outlook.com>
2021-03-25 10:50:52 +0530

    Updated age

--- lib/screens/large_screen/about.dart
+++ lib/screens/large_screen/about.dart
@@ -42,7 +42,7 @@ class _AboutScreenState extends State<AboutScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      "I am a 20-year-old Computer Science Undergrad from New Delhi."
+                      "I am a 21-year-old Computer Science Undergrad from New Delhi."
                       "\nThe foci of my projects vary from building software solutions to developing ideas and doing research, but what I prize myself for is the ability to learn new things and then building upon them."
                       "\nMy aspirations in my life are centred around empowering people, and striving for inclusive and dynamic answers to the world’s problems.",
                       style: GoogleFonts.montserrat(
