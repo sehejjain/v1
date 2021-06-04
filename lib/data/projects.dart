1d868db06d6dc698a62bff5bf9a0329e33a1e30e
Sehej Jain <sehejjain@outlook.com>
2021-06-04 11:36:24 +0530

    Added Projects data

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -5,14 +5,15 @@ Map<String, dynamic> projects = {
       "title": "Empowering the Urban Poor",
       "subtitle":
           "Bay Area Global Health Innovation Challenge, Top 16 Finalists",
-      "team": "Ayushi Gupta, Sehej Jain, Swati Ramtilak and Sumedh Supe",
+      "team":
+          "Ayushi Gupta, Sehej Jain, Swati Ramtilak, Sumedh Supe and Rohit Thakur",
       "link": "http://www.bayareaglobalhealthchallenge.com/2021-finalists",
       "quote":
           "\"Trust our vision for us to return it back with our service.\"",
     },
     {
       "name": "Brain Stroke",
-      "title": "Coming Soon",
+      "title": "Research Project",
       // "subtitle":
       //     "Bay Area Global Health Innovation Challenge, Top 16 Finalists",
       "team":
@@ -30,17 +31,26 @@ Map<String, dynamic> projects = {
       "subtitle":
           "Aim to build a web app that uses a Natural Language Processing Model to rank YouTube videos of different categories using weighted ranking of their comments",
       "team": "Aman Panjwani, Samarth Soni and Sehej Jain",
+      "link": "https://yourank-youtube.herokuapp.com/",
     },
     {
       "name": "This is Us",
       "title": "Matching You to Your Cause",
       "subtitle":
           "Designed an app to connect volunteers to various volunteering opportunities. Earlier created in Django but later shifted to Flutter to create an Android, iOS and Web Portal.",
+      "link": "https://github.com/sehejjain/this-is-us-Flutter"
     },
     {
       "name": "Deep learning based Community Detection in Social Media",
       "subtitle":
           "Worked on already published research papers related to Community Detection and using Deep Learning and AutoEncoders in tandem to create an efficient and accurate Deep Neural Network for detecting Communities.",
+    },
+    {
+      "name": "Indian Air Quality",
+      "subtitle":
+          "Analysis, Visualization, Prediction and Classification using ML Techniques",
+      "subsubtitle":
+          "Designed a two step ML pipeline that first calculated the Air Quality Index and then classified the Quality into states.",
     },
     // {
     //   "name": "Sirhaana",
