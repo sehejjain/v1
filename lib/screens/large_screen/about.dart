f3864f7eaaf36f97abc2edc31ea656756c3ec278
Sehej Jain <sehejjain@outlook.com>
2021-06-04 11:38:46 +0530

    Updated About Page

--- lib/screens/large_screen/about.dart
+++ lib/screens/large_screen/about.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:portfolio/components/social_media_icon.dart';
-import 'package:portfolio/models/icon_data.dart';
 
 class AboutScreen extends StatefulWidget {
   @override
